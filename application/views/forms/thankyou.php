cfb0aa81506bbca16868a90393ec5247e151d2fd
Hans Schoenburg <hschoenburg@gmail.com>
2012-05-19 12:54:54 -0400

    Improvements to thank you form. People controller saves thank your. Still need to implement reading and displaying

--- application/views/forms/thankyou.php
+++ application/views/forms/thankyou.php
@@ -1,29 +1,19 @@
 <div id ='thankyouwrapper'>
-<form name = 'thankyou' id='thankyouform' method='post' action='thankyou'>
+<form name = 'thankyou' id='thankyouform' method='post' action=''>
 	<p>
 		Thank you note for: <span id='reviewed' class='thank_title'></span>
 	</p>
-	<p>
-		<label for='gift'>What did they give you?</label>
+	<!--<p>
+		<label for='gift'>What did they give you? (brief title)</label>
 	</p>
 	<p>
-		<input type='text' class='big-border' maxlength ='100' name='thankyou_gift' id='thankyou_gift' value='' class='required'/>
+		<input type='text' class='big-border' name='gift' id='thankyou_gift' value='' class='required'/>
+	</p> -->
+	<p>
+	<label for='body'>Be sure to describe the gift.</label>
 	</p>
 	<p>
-		<label for='rating_select'>How would you rate the experience?</label>	
-	</p>
-	<p>
-		<select name='rating_select' id='rating_select' class='required'>
-			<option value='positive'>Positive</option>
-			<option value='neutral'>Neutral</option>
-			<option value='negaitve'>Negative</option>
-		</select>
-	</p>
-	<p>
-	<label for='body'>Write a thank you note:</label>
-	</p>
-	<p>
-		<textarea rows='5' class='big-border' name='body' id='body' value='' class='required'/></textarea>
+		<textarea rows='5' class='big-border' name='body' id='body' value='' class='required'/>
 	</p>
 	<!-- hidden fields -->
 	<input type='hidden' id='reviewed_id' name='reviewed_id' value=''/>
