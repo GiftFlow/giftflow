39b21bcc48d7c812c5c579ce4621e849ad1b5ee7
Jono <jono@foodnotblogs.com>
2012-08-09 06:55:44 -0400

    updated readme

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -18,21 +18,3 @@ $config['recaptcha'] = array(
    'RECAPTCHA_SIGNUP_URL' => 'https://www.google.com/recaptcha/admin/create',
    'theme' => 'clean'
  );
-/*
-$config['recaptcha'] = array(
-  'public'=>'6Ldy3rkSAAAAACVCSLSmiVy-IKnlAZpy5qtGNnOz',
-  'private'=>'6Ldy3rkSAAAAABfOMn91grUUznEv9i_pIB9_iBqK',
-  'RECAPTCHA_API_SERVER' =>'http://api.recaptcha.net',
-  'RECAPTCHA_API_SECURE_SERVER'=>'https://api-secure.recaptcha.net',
-  'RECAPTCHA_VERIFY_SERVER' =>'api-verify.recaptcha.net',
-  'theme' => 'clean'
-);
-
-$config['recaptcha'] = array(
-  'public'=>'6Lf0WboSAAAAAKlmGWsbyD2EEfzhkOexVsQh6c0B',
-  'private'=>'6Lf0WboSAAAAANelbkGzydg2QOf4aY3wuVprBk09',
-  'RECAPTCHA_API_SERVER' =>'http://api.recaptcha.net',
-  'RECAPTCHA_API_SECURE_SERVER'=>'https://api-secure.recaptcha.net',
-  'RECAPTCHA_VERIFY_SERVER' =>'api-verify.recaptcha.net',
-  'theme' => 'clean'
-);*/
