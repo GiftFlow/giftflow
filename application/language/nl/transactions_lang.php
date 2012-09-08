d5935c10264cb64397b34b2675f7482318cd2f54
Brandon Jackson <brandonscottjackson@gmail.com>
2012-09-08 18:47:05 -0400

    unused english language files deleted, default english language files copied to folders for all other languages to prevent errors (translation still needed)

---
+++ application/language/nl/transactions_lang.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+*	Transaction-related strings
+*/
+$lang["transaction_messages_section_title"] = "Messages";
+$lang["transaction_messages_instructions"] = "Send a Message";
+
+$lang["transaction_history_section_title"] = "History";
+$lang["transaction_history_transaction_new"] = "Transaction started";
+$lang["transaction_history_transaction_cancelled"] = "Transaction cancelled";
+$lang["transaction_history_transaction_declined"] = "Transaction declined";
+$lang["transaction_history_transaction_activated"] = "Transaction status changed to active";
+$lang["transaction_history_transaction_message"] = "Message sent";
