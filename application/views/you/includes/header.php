d37bf319f9e17509e953a4d1f814e0b627e9ad88
Hans Schoenburg <hschoenburg@gmail.com>
2012-09-17 17:11:55 -0400

    userdata change fixes

--- application/views/you/includes/header.php
+++ application/views/you/includes/header.php
@@ -1,6 +1,6 @@
 <div id='dash_header'>
 	<a href="<?php echo site_url("you");?>" class="user_image medium left">
-		<img src="<?php echo $userdata['photo_thumb_url'];?>" alt="<?php echo $userdata['screen_name'];?>" />
+		<img src="<?php echo $userdata['default_photo_thumb_url'];?>" alt="<?php echo $userdata['screen_name'];?>" />
 	</a>
 	<img src="<?php echo base_url()."assets/images/headings/your_dashboard.png";?>" alt="Your Dashboard" class="heading" />	
 	
