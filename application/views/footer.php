acc6a6c4e671d47f1ec1dbe1c4d7d3d737c0edeb
Hans Schoenburg <hschoenburg@gmail.com>
2012-10-04 16:57:20 -0400

    removed addthis analytics tracker

--- application/views/footer.php
+++ application/views/footer.php
@@ -38,7 +38,6 @@
 <script type="text/javascript" src="<?php echo base_url();?>assets/javascript/bootstrap.min.js"></script>
 
 <?php if(!empty($addthis) && $addthis == TRUE){ ?>
-	<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
 	<script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=giftflow"></script>
 <?php } ?>
 
