0635c193b57bb91fe5dd24de9d749425e5b6ea8f
Hans Schoenburg <hschoenburg@gmail.com>
2012-11-05 19:11:57 -0500

    minor test fix

--- application/libraries/Alert.php
+++ application/libraries/Alert.php
@@ -110,7 +110,7 @@ class Alert {
 		$footer = $this->CI->load->view('email/footer',FALSE,TRUE);
 		$config['message_html'] = $header.$this->message.$footer;
 		
-		$config["to"] = 'hans@giftflow.org';
+		$config["to"] = $this->to;
 		$config["reply_to_email"] = '';
 		$config["reply_to_name"] = '';
 		$config["cc"] = '';
