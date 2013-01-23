ca867beb589882516ea91c53654e90700ebb0307
Hans Schoenburg <hschoenburg@gmail.com>
2013-01-23 10:28:55 -0500

    heatmap analytics removed

--- application/views/footer.php
+++ application/views/footer.php
@@ -103,15 +103,6 @@ $(function(){
 	}
 });
 
-/*Crazy Egg heatmap analytics */
-setTimeout(function(){
-	var a=document.createElement("script");
-	var b=document.getElementsByTagName("script")[0];
-	a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0013/6748.js?"+Math.floor(new Date().getTime()/3600000);
-	a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)
-	}, 1);
-
-
 /* Scripts for header menu items */
 $('.dropdown-menu').find('form').click(function(e) {
 	e.stopPropagation();
